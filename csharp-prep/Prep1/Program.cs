5f8b6f51ad39484c493c533af3febd8efc7fca37
bloominolive <bloomin.olive88@gmail.com>
2023-01-02 21:21:55 -0500

    prep1 complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        var FirstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        var LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
     }
 }
