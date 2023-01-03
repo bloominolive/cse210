bdd72b222c1657f7410a5e355960c756162978ff
bloominolive <bloomin.olive88@gmail.com>
2023-01-02 22:02:08 -0500

    prep2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percentageInt = int.Parse(percentage);
+        string letter;
+        string sign = "";
+
+        if (percentageInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentageInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentageInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentageInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int number = percentageInt/10;
+        int remainder = percentageInt%10;
+
+
+        if (remainder >= 7 && number < 9 && number > 5)
+        {
+            sign = "+";
+        }
+        else if (remainder <= 3 && number > 5 && number < 10)
+        {
+            sign = "-";
+        }
+
+        Console.Write($"Your letter grade is {letter}{sign}. ");
+
+        if (percentageInt >= 70)
+        {
+            Console.Write("You Passed!");
+        }
+        else
+        {
+            Console.Write("You shall not pass.");
+        }
     }
 }
