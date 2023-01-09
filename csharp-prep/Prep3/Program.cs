90fc8e72488a358f5c8b61c69a466830d1e39816
bloominolive <bloomin.olive88@gmail.com>
2023-01-09 06:43:45 -0500

    prep 3 activity complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   // for first requirement from user input
+       /*  Console.Write("What is the magic number? ");
+        string inputMagicNumber = Console.ReadLine();
+        int magicNumber = int.Parse(inputMagicNumber); */
+
+        // for second requirement with random number generator
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("Guess the magic number from 1-100!");
+
+        int numGuess = -1;
+
+        while (numGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string inputNumGuess = Console.ReadLine();
+            numGuess = int.Parse(inputNumGuess);
+
+            if (numGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (numGuess > magicNumber) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+        }
     }
 }
