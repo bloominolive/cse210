8551df29ba12d38fe30f0941d295fd96bece2cec
bloominolive <bloomin.olive88@gmail.com>
2023-01-30 05:47:46 -0500

    fraction assignment complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+       Fraction fraction1 = new Fraction();
+       fraction1.GetFractionString();
+       fraction1.GetDecimalValue();
+
+       Fraction fraction2 = new Fraction(5);
+       fraction2.GetFractionString();
+       fraction2.GetDecimalValue();
+
+       Fraction fraction3 = new Fraction(3,4);
+       fraction3.GetFractionString();
+       fraction3.GetDecimalValue();
+
+       Fraction fraction4 = new Fraction(1,3);
+       fraction4.GetFractionString();
+       fraction4.GetDecimalValue();
     }
 }
