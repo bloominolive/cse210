b3e3c08bfc6f08778c2fe09f55e2995a5df832cf
bloominolive <bloomin.olive88@gmail.com>
2023-02-14 06:49:42 -0500

    learning activity complete w7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Jones", "Europe");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Donald", "Calculus", "2", "1-4");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkdList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Johnson Johannson", "Autobiographies", "My Amazing Story");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
